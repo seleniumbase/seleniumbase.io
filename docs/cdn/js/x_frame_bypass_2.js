c09188d76138a95e651192a3bdf78c64b87a3ddd
Michael Mintz <mdmintz@gmail.com>
2020-10-22 19:38:20 -0400

    Update x_frame_bypass_2.js

--- docs/cdn/js/x_frame_bypass_2.js
+++ docs/cdn/js/x_frame_bypass_2.js
@@ -69,8 +69,8 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://api.codetabs.com/v1/proxy/?quest=',
-			'https://cors-anywhere.herokuapp.com/'
+			'https://cors-anywhere.herokuapp.com/',
+			'https://api.codetabs.com/v1/proxy/?quest='
 		]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
